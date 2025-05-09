ea7f14157c3f687eccf3f64dc95cfc92b2d2ecbc
akbarc <amchranya@gmail.com>
2025-05-09 14:30:12 -0400

    Add Meta Pixel tracking code to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ReservationBanner } from "@/components/reservation-banner"
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -14,6 +15,8 @@ const { baseMetadata } = require("./metadata") as { baseMetadata: Metadata }
 
 export const metadata: Metadata = {
   ...baseMetadata,
+  title: 'TeaHC - Move Freely Again Without Constant Discomfort',
+  description: 'TeaHC\'s fast-acting formulas work in 15-45 minutes, helping you enjoy your favorite activities again.',
 }
 
 export default function RootLayout({
@@ -23,6 +26,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1022859733312316');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1022859733312316&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <ReservationBanner />
